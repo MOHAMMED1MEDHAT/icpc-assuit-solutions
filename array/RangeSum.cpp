6f912c8f313efb83646b04b192790c157b077b11
mohammed medhat <mohamed.medhat2121@gmail.com>
2023-08-03 21:45:57 +0300

    Solved RangeSum 0.1

--- array/RangeSum.cpp
+++ array/RangeSum.cpp
@@ -5,22 +5,21 @@ int main(int argc, char const *argv[])
 {
     long long n, q;
     cin >> n >> q;
-    long long arr[n] = {0}, sum = 0;
+    long long arr[n] = {0}, freq[n] = {0}, sum = 0, res = 0;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+        sum += arr[i];
+        freq[i] = sum;
     }
     while (q--)
     {
         int startIdx, endIdx;
         cin >> startIdx >> endIdx;
         startIdx--, endIdx--;
-        for (int i = startIdx; i <= endIdx; i++)
-        {
-            sum += arr[i];
-        }
-        cout << sum << endl;
-        sum = 0;
+        res = (freq[endIdx] - freq[startIdx]) + arr[startIdx];
+        cout << res << endl;
+        res = 0;
     }
 
     return 0;
